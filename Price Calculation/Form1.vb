e8195b12cd4d0d1b7676e75808306456352e3185
user <user@LAPTOP-KAN0NU4I>
2023-09-06 11:44:32 +0200

    Add project files.

---
+++ Price Calculation/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnTotal.Click
+
+        Dim Price As Decimal
+        Dim Quantity As Integer
+        Dim Total As Decimal
+        Dim NameOfCustomer As String
+
+        Price = txtPrice.Text
+        Quantity = txtQuantity.Text
+        NameOfCustomer = txtNameOfCustomer.Text
+
+        Total = Price * Quantity
+
+        MsgBox("Hi " & NameOfCustomer & " , your Total is R" & Total)
+
+    End Sub
+End Class
